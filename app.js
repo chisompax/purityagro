d5ed9ddb9e5476515043e6113e6fd8b075b8b50f
Chisom <chisompax4@gmail.com>
2020-12-04 01:01:48 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+ScrollReveal().reveal('.vile');
+ScrollReveal().reveal('.riv', { delay: 500 })
+ScrollReveal().reveal('.scroll', { delay: 600 })
+ScrollReveal().reveal('.roll', { delay: 700 })
+
